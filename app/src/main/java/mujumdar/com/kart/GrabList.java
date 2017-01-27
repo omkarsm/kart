a32da97bc59030c25d6b26e739fb38157e5efe2a
Omkar Mujumdar <omkar.mujumdar@seastarlabs.com>
2017-01-27 20:19:54 +0530

    just a push

--- app/src/main/java/mujumdar/com/kart/GrabList.java
+++ app/src/main/java/mujumdar/com/kart/GrabList.java
@@ -4,6 +4,8 @@ import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Gravity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.RelativeLayout;
 import android.widget.ScrollView;
 import android.widget.TableLayout;
@@ -11,6 +13,8 @@ import android.widget.TableRow;
 import android.widget.TextView;
 
 public class GrabList extends AppCompatActivity {
+
+    private Integer count;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,8 +26,12 @@ public class GrabList extends AppCompatActivity {
         TableLayout computedLayout = new TableLayout(this);
         computedLayout.setLayoutParams(new TableLayout.LayoutParams(TableLayout.LayoutParams.WRAP_CONTENT, TableLayout.LayoutParams.MATCH_PARENT));
         TableRow computeRow = new TableRow(this);
+        Button addButton = new Button(this);
+        addButton.setText("ADD");
+        addButton.setLayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT, TableRow.LayoutParams.WRAP_CONTENT, 0.1f));
+        computeRow.addView(addButton);
         TextView totalLabel = new TextView(this);
-        totalLabel.setLayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT, TableRow.LayoutParams.WRAP_CONTENT, 0.7f));
+        totalLabel.setLayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT, TableRow.LayoutParams.WRAP_CONTENT, 0.6f));
         totalLabel.setText("Total");
         computeRow.addView(totalLabel);
         TextView totalValue = new TextView(this);
@@ -37,11 +45,26 @@ public class GrabList extends AppCompatActivity {
         ScrollView itemsView = new ScrollView(this);
         itemsView.setLayoutParams(new ScrollView.LayoutParams(ScrollView.LayoutParams.MATCH_PARENT, ScrollView.LayoutParams.MATCH_PARENT));
         itemsView.setFillViewport(true);
-        TableLayout itemsTable = new TableLayout(this);
+        final TableLayout itemsTable = new TableLayout(this);
         itemsTable.setId(Integer.parseInt(getString(R.string.item_list_id)));
         itemsView.addView(itemsTable);
         parentLayout.addView(itemsView);
-
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TableRow tr = new TableRow(GrabList.this);
+                TextView field = new TextView(GrabList.this);
+                field.setText("Item " + count);
+                field.setLayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT, TableRow.LayoutParams.WRAP_CONTENT, 0.7f));
+                tr.addView(field);
+                TextView amt = new TextView(GrabList.this);
+                amt.setText("Item " + count);
+                amt.setLayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT, TableRow.LayoutParams.WRAP_CONTENT, 0.7f));
+                tr.addView(amt);
+                itemsTable.addView(tr);
+                count++;
+            }
+        });
         /*parentLayout.addView(webView);
         setContentView(parentLayout);*/
     }
